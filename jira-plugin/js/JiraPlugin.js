a5bed2650358602064551f5d338a22bd0f058b8c
Andriy Blyschak <ably@ciklum.com>
2019-01-18 10:01:24 +0200

    [1.1.2] added impact analysis functionality

--- jira-plugin/js/JiraPlugin.js
+++ jira-plugin/js/JiraPlugin.js
@@ -16,19 +16,9 @@ function createArray(size, mapper = () => 1) {
 }
 
 const app = new Vue({
-  // render(createElement) {
-  //   return createElement(
-  //     'div',
-  //     {
-  //       class: {
-  //         'main-container': true,
-  //       },
-  //     },
-  //   );
-  // },
   template: `
   <div class="main-container">
-    <div class="main-menu" :style="{ '--menu-turns-count': 2 }">
+    <div class="main-menu" :style="{ '--menu-turns-count': 1 }">
       <transition-group name="menu">
         <CircleButton
           v-if="show"
@@ -57,6 +47,46 @@ const app = new Vue({
         @click="toggleMenu"
       />
       <CreateBranch />
+      <div class="ju-modal" ref="modal">
+        <transition
+          name="subwindow"
+          @after-enter="afterEnter"
+          @after-leave="afterLeave"
+        >
+          <div v-if="modal.show" class="ju-modal__overlay">
+            <div class="ju-modal__wrapper">
+              <div class="ju-modal__inner">
+                <div class="ju-modal__container">
+                  <h1 class="ju-modal__title">{{ modal.title }}</h1>
+                  <div
+                    class="ju-modal__list-item"
+                    v-for="(input, index) in modal.inputs"
+                    :key="index"
+                  >
+                    <span v-if="impactSurvey.multiple" class="impact-marker"></span>
+                    <input
+                      type="text"
+                      class="ju-modal__value"
+                      v-model="input.value"
+                      @keyup.enter="modal.show = false"
+                    >
+                  </div>
+                  <RectangleButton
+                    v-if="impactSurvey.multiple"
+                    color="blue"
+                    text="Add item"
+                    class="add-impact-value"
+                    @click="addModalValue"
+                  />
+                  <br />
+                  <RectangleButton color="green" text="Ok" @click="modal.show = false" />
+                  <RectangleButton color="red" text="Skip" @click="hideModal" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
     </div>
   </div>`,
 
@@ -97,6 +127,83 @@ const app = new Vue({
         },
       ],
       commitMessage: '',
+      modal: {
+        title: '',
+        inputs: [
+          {
+            value: '',
+          },
+        ],
+        show: false,
+      },
+      impactValue: {
+        version: ' ',
+        feature: ' ',
+        brands: ' ',
+        checkList: ' ',
+      },
+      impactSurvey: {
+        questions: [
+          {
+            title: 'Version / env',
+            value: '',
+            start(component) {
+              component.showModal(this.title, this.value);
+            },
+            after(component, inputs) {
+              component.impactValue.version = inputs[0].value || ' ';
+              component.impactSurvey.step += 1;
+              component.impactSurvey.questions[component.impactSurvey.step].start(component);
+            },
+          },
+          {
+            title: 'Features modified',
+            value: '',
+            start(component) {
+              component.showModal(this.title, this.value);
+            },
+            after(component, inputs) {
+              component.impactValue.feature = inputs[0].value || ' ';
+              component.impactSurvey.step += 1;
+              component.impactSurvey.questions[component.impactSurvey.step].start(component);
+            },
+          },
+          {
+            title: 'Brands /Features affected',
+            value: '',
+            start(component) {
+              component.showModal(this.title, this.value);
+            },
+            after(component, inputs) {
+              component.impactValue.brands = inputs[0].value || ' ';
+              component.impactSurvey.step += 1;
+              component.impactSurvey.questions[component.impactSurvey.step].start(component);
+            },
+          },
+          {
+            title: 'What\'s need to be checked (recommendations from developer)',
+            value: '',
+            start(component) {
+              component.showModal(this.title, this.value);
+              component.impactSurvey.multiple = true;
+            },
+            after(component, inputs) {
+              component.impactValue.checkList = inputs.length > 1
+                                                ? inputs.reduce(
+                  (str, item, index) => `${str}${index > 0 ? '\n' : ''} * ${item.value}`,
+                  '',
+                )
+                                                : inputs[0].value || ' ';
+              component.impactSurvey.step = 0;
+              component.impactSurvey.multiple = false;
+              component.addImpact();
+            },
+          },
+        ],
+        step: 0,
+        multiple: false,
+      },
+      impactAnalysisTemplate: '||Summary||Description||\n|*Version / env*|{version}|\n|*Features modified*|{feature}|\n|*Brands /Features affected*|{brands}|\n|*What\'s need to be checked (recommendations from developer)*|{checkList}|',
     };
   },
 
@@ -104,15 +211,76 @@ const app = new Vue({
     chrome.storage.sync.get(['jiraPluginSettings'], (storage) => {
       this.settings = storage.jiraPluginSettings;
       this.buttons.forEach((value, index) => {
-        this.createAnimation('showMenuItem', 'hideMenuItem', index, 90 + this.getRotationDegree(index));
+        this.createAnimation(
+          'showMenuItem',
+          'hideMenuItem',
+          index,
+          90 + this.getRotationDegree(index)
+        );
       });
     });
-  },
-
-  mounted() {
+    this.$root.$on('addImpact', this.showImpactSurvey);
   },
 
   methods: {
+    addModalValue() {
+      this.modal.inputs.push({ value: '' });
+    },
+
+    showModal(title, value) {
+      this.modal.title = title;
+      this.modal.inputs.splice(1);
+      this.modal.inputs[0].value = value;
+      this.modal.show = true;
+    },
+
+    hideModal() {
+      this.impactSurvey.step = this.impactSurvey.questions.length;
+      this.modal.show = false;
+    },
+
+    afterEnter() {
+      this.$refs.modal.querySelector('.ju-modal__value').focus();
+    },
+
+    afterLeave() {
+      if (this.impactSurvey.questions.length > this.impactSurvey.step) {
+        this.impactSurvey.questions[this.impactSurvey.step].after(this, this.modal.inputs);
+      } else {
+        this.impactSurvey.step = 0;
+        this.addImpact();
+      }
+    },
+
+    showImpactSurvey() {
+      this.impactSurvey.questions[this.impactSurvey.step].start(this);
+    },
+
+    addImpact() {
+      const issueId = location.pathname.match(/\w+-\d+$/)[0];
+      fetch(`https://jira.betlab.com/rest/api/2/issue/${issueId}/comment`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          body: this.enrichTemplate(),
+        }),
+      }).then(() => {
+        const script = document.createElement('script');
+        script.innerHTML = 'JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()])';
+        document.body.appendChild(script);
+      }).catch(console.error);
+    },
+
+    enrichTemplate() {
+      return this.impactAnalysisTemplate
+                 .replace('{version}', this.impactValue.version)
+                 .replace('{feature}', this.impactValue.feature)
+                 .replace('{brands}', this.impactValue.brands)
+                 .replace('{checkList}', this.impactValue.checkList);
+    },
+
     showTooltip(button, event) {
       this.$root.$emit('showTooltip', event.target, button.label);
     },
@@ -127,7 +295,7 @@ const app = new Vue({
       index,
       startTurn = 90,
       frames = 100,
-      turnRate = 360 * this.settings.turnCount,
+      turnRate = 360,
     ) {
       const startRadius = 0;
       const endRadius = ((this.buttons.length * 48) * 4) / Math.PI / 2;
